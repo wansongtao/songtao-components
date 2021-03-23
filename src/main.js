a0c7f655ab64184fadd6275bd84fc2d12ca67b5f
万松涛 <2294215581@qq.com>
2021-03-23 22:51:17 +0800

    使用svg给html元素添加水印

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.directive('water-mark', (el) => {
+  el.style.backgroundImage = `url("data:image/svg+xml,${encodeURIComponent(`<svg width='120' height='120' xmlns='http://www.w3.org/2000/svg'><text transform='translate(60,60) rotate(-45)' font-size='16' fill='#DDD' text-anchor='middle' dominant-baseline='middle'>${'wst'}</text></svg>`)}")`
+})
+
 Vue.config.productionTip = false
 
 new Vue({
